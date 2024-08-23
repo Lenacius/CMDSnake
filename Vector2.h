60ae9af18219b79deca2ee7b967120030fb09833
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2024-08-22 23:19:59 -0300

    Added comparison operators for Vector2

--- Vector2.h
+++ Vector2.h
@@ -11,4 +11,11 @@ struct Vector2 {
 		this->x = x;
 		this->y = y;
 	}
+	friend bool operator==(const Vector2& val1, const Vector2& val2) {
+		return (val1.x == val2.x
+				and val1.y == val2.y);
+	}
+	friend bool operator!=(const Vector2& val1, const Vector2& val2) {
+		return !(val1 == val2);
+	}
 };

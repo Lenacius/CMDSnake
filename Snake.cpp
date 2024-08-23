c5a55d81e03ea1e972b8b22de6bc9aede7e9da91
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2024-08-22 22:14:35 -0300

    Added drawing of initial board

--- Snake.cpp
+++ Snake.cpp
@@ -1,11 +1,21 @@
 #include "Snake.h"
 
 void Snake::initialize() {
+	board_size_x = 10;
+	board_size_y = 10;
 
+	for (int x = 0; x < board_size_x; x++) {
+		for (int y = 0; y < board_size_y; y++) {
+			if(	(y == 0 or y == board_size_y - 1)
+				or (x == 0 or x == board_size_x - 1) ){
+				Renderer::render_at_position(x, y, 'X');
+			}
+		}
+	}
 }
 
 void Snake::main_loop() {
-
+	game_status = GameStatus::END;
 }
 
 void Snake::finish() {
--- Snake.h
+++ Snake.h
@@ -5,6 +5,10 @@
 using namespace std;
 
 class Snake : public Game {
+private:
+	int board_size_x;
+	int board_size_y;
+
 protected:
 	void initialize() override;
 	void main_loop() override;

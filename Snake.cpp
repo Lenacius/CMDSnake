01123e36c3b81ea61e4c87504968c4a26b6f0917
Lenacius Grimpel <39158258+Lenacius@users.noreply.github.com>
2024-08-22 22:25:24 -0300

    Added default constructor for Snake and Vector2
    Created snake obj for rendering

--- Snake.cpp
+++ Snake.cpp
@@ -1,8 +1,11 @@
 #include "Snake.h"
 
-void Snake::initialize() {
+Snake::Snake() {
 	board_size = { 10, 10 };
+	_snake = { 5, 5 };
+}
 
+void Snake::initialize() {
 	for (int x = 0; x < board_size.x; x++) {
 		for (int y = 0; y < board_size.y; y++) {
 			if(	(y == 0 or y == board_size.y - 1)
@@ -11,6 +14,8 @@ void Snake::initialize() {
 			}
 		}
 	}
+
+	Renderer::render_at_position(_snake.x, _snake.y, 'O');
 }
 
 void Snake::main_loop() {
--- Snake.h
+++ Snake.h
@@ -7,6 +7,10 @@ using namespace std;
 struct Vector2 {
 	int x;
 	int y;
+	Vector2() {
+		x = 0;
+		y = 0;
+	}
 	Vector2(int x, int y) {
 		this->x = x;
 		this->y = y;
@@ -16,9 +20,13 @@ struct Vector2 {
 class Snake : public Game {
 private:
 	Vector2 board_size;
+	Vector2 _snake;
 
 protected:
 	void initialize() override;
 	void main_loop() override;
 	void finish() override;
+
+public:
+	Snake();
 };
